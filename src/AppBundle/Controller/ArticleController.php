a170a6d78ac0a96038dcda6432b1a71085c2897e
Qtarantino <weider;quentin@gmail.com>
2017-08-07 10:39:53 +0200

    ajout classe Article, controller et formulaire

--- src/AppBundle/Controller/ArticleController.php
+++ src/AppBundle/Controller/ArticleController.php
@@ -1,15 +1,66 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Quentin
- * Date: 07/08/2017
- * Time: 09:34
- */
+
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Article;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class ArticleController
+class ArticleController extends Controller
 {
+    /**
+     * @Route("/taxes", name="taxes")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
+     * @throws \LogicException
+     */
+    public function applicationAction(Request $request)
+    {
+        $article = new Article();
 
+        $form = $this->createFormBuilder()
+            ->add('nom', TextType::class)
+            ->add('description', TextareaType::class, array(
+                'required' => false
+            ))
+            ->add('montant_ht', NumberType::class, array(
+                'scale' => 2,
+                'attr' => array(
+                    'min' => 0
+                )
+            ))
+            ->add('valider', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $data = $form->getData();
+
+            $prixHT = $data['montant_ht'];
+
+            $article->setAmountHT($prixHT);
+            $article->setNom($data['nom']);
+            $article->setDescription($data['description']);
+            $article->setCreation(new \DateTime('now'));
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($article);
+            $em->flush();
+
+            return $this->redirect($request->getUri());
+        }
+
+        return $this->render(
+            'default/taxes.html.twig',
+            array('form' => $form->createView())
+        );
+    }
 }
---
+++ src/AppBundle/Entity/Article.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Article
+ *
+ * @ORM\Table(name="article")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ArticleRepository")
+ */
+class Article
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=100, unique=true)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255, nullable=true)
+     */
+    private $description;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="amountHT", type="float")
+     */
+    private $amountHT;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="creation", type="datetime")
+     */
+    private $creation;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Article
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Article
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set amountHT
+     *
+     * @param float $amountHT
+     *
+     * @return Article
+     */
+    public function setAmountHT($amountHT)
+    {
+        $this->amountHT = $amountHT;
+
+        return $this;
+    }
+
+    /**
+     * Get amountHT
+     *
+     * @return float
+     */
+    public function getAmountHT()
+    {
+        return $this->amountHT;
+    }
+
+    /**
+     * Set creation
+     *
+     * @param \DateTime $creation
+     *
+     * @return Article
+     */
+    public function setCreation($creation)
+    {
+        $this->creation = $creation;
+
+        return $this;
+    }
+
+    /**
+     * Get creation
+     *
+     * @return \DateTime
+     */
+    public function getCreation()
+    {
+        return $this->creation;
+    }
+}
+
